28e45190ac0ca086c2f0509891b51b0f535d3fbc
Yushirizu <113351099+Yushirizu@users.noreply.github.com>
2022-12-25 22:18:18 +0100

    made the file unable to see maybe idk

--- timer.js
+++ timer.js
@@ -1,35 +1,32 @@
 window.onload = function() {
-  // Make a request to the YouTube API to retrieve the date and time of the last stream
-  fetch('https://www.googleapis.com/youtube/v3/activities?part=snippet,contentDetails&maxResults=1&channelId=UC3n5uGu18FoCy23ggWWp8tA&type=upload&key=AIzaSyAFhUpDReRLp_TL71eRvzjL7GY2J5Y3sq4')
-    
-    .then(response => response.json())
-    .then(data => {
-      
-      
-      // Parse the response to extract the date and time of the last stream
-      const lastStreamDate = new Date(data.items[0].snippet.publishedAt);
 
-      console.log(lastStreamDate);
+  const url = `https://www.googleapis.com/youtube/v3/activities?part=snippet,contentDetails&maxResults=1&channelId=UC3n5uGu18FoCy23ggWWp8tA&type=upload&key=${API_KEY}`;
 
-      // Set up a timer to update the countdown every second
-      setInterval(function() {
-        // Calculate the difference between the current time and the date and time of the last stream
-        const timeDifference = Date.now() - lastStreamDate.getTime();
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    // Parse the response to extract the date and time of the last stream
+    const lastStreamDate = new Date(data.items[0].snippet.publishedAt);
 
-        // Calculate the number of days, hours, minutes, and seconds
-        const secondsInADay = 60 * 60 * 1000 * 24;
-        const secondsInAHour = 60 * 60 * 1000;
-        const days = Math.floor(timeDifference / (secondsInADay) * 1);
-        const hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
-        const mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
-        const secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+    // Set up a timer to update the countdown every second
+    setInterval(function() {
+      // Calculate the difference between the current time and the date and time of the last stream
+      const timeDifference = Date.now() - lastStreamDate.getTime();
 
-        // Update the countdown elements on the page with the updated values
-        document.getElementById('days').innerHTML = days;
-        document.getElementById('hours').innerHTML = hours;
-        document.getElementById('minutes').innerHTML = mins;
-        document.getElementById('seconds').innerHTML = secs;
-        
-      }, 1000);
-    });
-};
+      // Calculate the number of days, hours, minutes, and seconds
+      const secondsInADay = 60 * 60 * 1000 * 24;
+      const secondsInAHour = 60 * 60 * 1000;
+      const days = Math.floor(timeDifference / (secondsInADay) * 1);
+      const hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
+      const mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
+      const secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+
+      // Update the countdown elements on the page with the updated values
+      document.getElementById('days').innerHTML = days;
+      document.getElementById('hours').innerHTML = hours;
+      document.getElementById('minutes').innerHTML = mins;
+      document.getElementById('seconds').innerHTML = secs;
+    }, 1000);
+  });
+}
+
